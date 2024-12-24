379d0346cd6ee213603619a1ed75edbe93e5a7b5
Dalip Tandon <daliptandon8@gmail.com>
2024-12-24 22:33:49 +0530

    footer done

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,8 @@
 "use client"
+import CallToAction from "./sections/CallToAction";
+import Faqs from "./sections/Faqs";
 import Features from "./sections/Features";
+import Footer from "./sections/Footer";
 import Hero from "./sections/Hero";
 import Integrations from "./sections/Integrations";
 import Introduction from "./sections/Introduction";
@@ -15,6 +18,9 @@ export default function Home() {
         <Introduction/>
         <Features/>
         <Integrations/>
+        <Faqs/>
+        <CallToAction/>
+        <Footer/>
     </>
   );
 }
