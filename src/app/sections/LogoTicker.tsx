5655b8bb0cb9e8ecae866da7daf9239a4565c505
Dalip Tandon <daliptandon8@gmail.com>
2024-12-25 19:41:23 +0530

    introduction animation

--- src/app/sections/LogoTicker.tsx
+++ src/app/sections/LogoTicker.tsx
@@ -8,6 +8,8 @@ import apexLogo from "../assets/images/apex.svg";
 import celestialLogo from "../assets/images/celestial.svg";
 import twiceLogo from "../assets/images/twice.svg";
 import Image from "next/image";
+import { Fragment } from "react";
+import { AnimatePresence,motion } from "framer-motion";
 const logos = [
     { name: "Quantum", image: quantumLogo },
     { name: "Acme Corp", image: acmeLogo },
@@ -23,12 +25,17 @@ export default function LogoTicker() {
     return<section className="py-24 overflow-x-clip" >
         <div className="container">
         <h3 className="text-center text-white/50 text-xl">Already choosen by these market leaders</h3>
-        <div className="overflow-hidden mt-12 [mask-image:linear-gradient(to_right,_transparent,_black_10%,_black_90%,_transparent)]">
-            <div className="flex gap-24 pr-24">
-                {logos.map((logo)=>(
+        <div className="flex overflow-hidden mt-12 [mask-image:linear-gradient(to_right,_transparent,_black_10%,_black_90%,_transparent)]">
+            <motion.div animate={{x:"-50%"}} transition={{duration:30,ease:"linear",repeat:Infinity}} className="flex flex-none gap-24 pr-24">
+                {Array.from({length:2}).map((_,i)=>(
+                    <Fragment key={i}>
+                       {logos.map((logo)=>(
                     <Image src={logo.image} key={logo.name} alt={logo.name}/>
-                ))} 
-            </div>
+                ))}
+                    </Fragment>
+                ))}
+              
+            </motion.div>
         </div>
         </div>
     </section>
